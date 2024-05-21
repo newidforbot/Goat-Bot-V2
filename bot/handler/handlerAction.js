7e1ae25f090414390680546d94e9593086aef023
deepamhonih <169180265+newidforbot@users.noreply.github.com>
2024-05-21 18:25:16 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,7 +44,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "😈"){
-  if(event.userID == "61559640731665"){
+  if(event.userID == "100039655626737"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -54,7 +54,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61559640731665"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100039655626737"){
     message.unsend(event.messageID)
 }else{
     message.send("")
